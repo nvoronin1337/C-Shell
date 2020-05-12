a201a169504e704f51510f7b46042573c79d5043
Nikita Voronin <voro0612@gmail.com>
2020-05-12 15:24:53 +0300

    Added batch mode

--- shell2.c
+++ shell2.c
@@ -10,12 +10,30 @@
 #include <sys/wait.h>
 
 #define EXIT_USER 5
+#define QUIT "quit"
 
-int parse_input(char *, char **);
-void parse_commands(int, char **);
+int parse_input_line(char *, char **);
+void run_commands(int, char **);
 void execute(char **);
+void remove_spaces(char *);
+int read_file(FILE*, char **);
 
-int parse_input(char* input, char** commands){
+int read_file(FILE* file, char **lines){
+    char* file_input = NULL;
+    size_t len = 0;
+    ssize_t lineSize = 0;
+
+    int i = 0;
+    
+    while(lineSize != -1){
+        lineSize = getline(&lines[i], &len, file);
+        i++;
+    }
+
+    return i;
+}
+
+int parse_input_line(char* input, char** commands){
     size_t ln = strlen(input) - 1;
 
     if (*input && input[ln] == '\n') 
@@ -33,10 +51,14 @@ int parse_input(char* input, char** commands){
         i++;
     }
 
+    if(command){
+        free(command);
+        command = NULL;
+    }
     return i;
 }
 
-void parse_commands(int size, char** commands){
+void run_commands(int size, char** commands){
     const char* ARG_SEPARATOR = " ";
 
     char** args = malloc(512 * sizeof(char *));
@@ -47,18 +69,22 @@ void parse_commands(int size, char** commands){
 
         int l = 0;
         while(arg){
+            remove_spaces(arg);
+
+            if(!strcmp(arg, QUIT)){
+                exit(EXIT_USER);
+            }
             args[l] = arg;
             l++;
             arg = strtok(NULL, ARG_SEPARATOR);
         }
+        
         args[l] = NULL;
-        execute(args);    
+        execute(args);   
+        printf("/n"); 
     }
     free(args);
     args = NULL;
-
-    free(commands);
-    commands = NULL;
 }
 
 void execute(char** command){
@@ -84,12 +110,21 @@ void execute(char** command){
     }
 }
 
+void remove_spaces(char* s) {
+    const char* d = s;
+    do {
+        while (*d == ' ') {
+            ++d;
+        }
+    } while (*s++ = *d++);
+}
+
 int main(int argc, char* argv[]){
-    if(argc > 1){
-        printf("batch mode\n");
-    }else{
+    switch(argc){
+    case 1:
+    {
+        char* input = NULL;
         while(1){
-            char* input = NULL;
             size_t len = 0;
             ssize_t lineSize = 0;
 
@@ -97,18 +132,45 @@ int main(int argc, char* argv[]){
             lineSize = getline(&input , &len, stdin);
 
             if(lineSize-1 != 0){
-                if(!strcmp(input, "quit\n")){
-                    return(EXIT_USER);
-                }
-
                 char** commands = malloc(512 * sizeof(char *));
-                int size = parse_input(input, commands);
-                parse_commands(size, commands);
+                int size = parse_input_line(input, commands);
+                run_commands(size, commands);
+                free(commands);
+                commands = NULL;
+            }
+        }
+        break;
+    }
+    case 2:
+    {
+        char** lines = malloc(128 * sizeof(char *));
+        char** commands = malloc(512 * sizeof(char *));
+
+        FILE* file;
+        if(file = fopen(argv[1], "r")){
+            int total_lines = read_file(file, lines);
+           
+            for(int i = 0; i < total_lines; i++){           
+                int commands_in_line = parse_input_line(lines[i], commands);
+                run_commands(commands_in_line, commands);
             }
 
-            free(input);
-            input = NULL;
+            fclose(file);
+        }else{
+            perror("File not found!\n");
         }
+
+        free(lines);
+        lines = NULL;
+
+        free(commands);
+        commands = NULL;
+
+        break;
+    }
+    default:
+        perror("Invalid number of arguments!\n");
+        return(EXIT_FAILURE);
     }
     return(EXIT_SUCCESS);
 }
